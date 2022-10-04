4db698f33aa66ab1aca6333039adc61559acdb59
Pedro Moreno <pedro.moreno@saleslayer.com>
2022-10-04 10:44:16 +0200

    Updated version 2.5.8:
    - Optimized index names when processing variants as products.

--- Saleslayer/Synccatalog/Model/Synccatalog.php
+++ Saleslayer/Synccatalog/Model/Synccatalog.php
@@ -1167,8 +1167,8 @@ class Synccatalog extends \Magento\Framework\Model\AbstractModel{
     */
     private function prepare_product_data_to_store($arrayProducts){
 
-        $fixed_product_fields = array('ID', 'ID_catalogue', 'url_key', $this->product_field_name, $this->product_field_description, $this->product_field_description_short, $this->product_field_price, $this->product_field_image, 'image_sizes', $this->product_field_sku, $this->product_field_qty, $this->product_field_attribute_set_id, $this->product_field_meta_title, $this->product_field_meta_keywords, $this->product_field_meta_description, $this->product_field_length, $this->product_field_width, $this->product_field_height, $this->product_field_weight, $this->product_field_related_references, $this->product_field_crosssell_references, $this->product_field_upsell_references, $this->product_field_inventory_backorders, $this->product_field_inventory_min_sale_qty, $this->product_field_inventory_max_sale_qty, $this->product_field_status, $this->product_field_visibility, $this->product_field_tax_class_id, $this->product_field_country_of_manufacture, $this->product_field_special_price, $this->product_field_special_from_date, $this->product_field_special_to_date);
-
+        $fixed_product_fields = array('ID', 'ID_catalogue', 'ID_products', 'url_key', $this->product_field_name, $this->product_field_description, $this->product_field_description_short, $this->product_field_price, $this->product_field_image, 'image_sizes', $this->product_field_sku, $this->product_field_qty, $this->product_field_attribute_set_id, $this->product_field_meta_title, $this->product_field_meta_keywords, $this->product_field_meta_description, $this->product_field_length, $this->product_field_width, $this->product_field_height, $this->product_field_weight, $this->product_field_related_references, $this->product_field_crosssell_references, $this->product_field_upsell_references, $this->product_field_inventory_backorders, $this->product_field_inventory_min_sale_qty, $this->product_field_inventory_max_sale_qty, $this->product_field_status, $this->product_field_visibility, $this->product_field_tax_class_id, $this->product_field_country_of_manufacture, $this->product_field_special_price, $this->product_field_special_from_date, $this->product_field_special_to_date);
+        
         $product_data_to_store = array();
 
         $product_data_to_store['avoid_stock_update'] = $this->avoid_stock_update;
@@ -9921,10 +9921,27 @@ class Synccatalog extends \Magento\Framework\Model\AbstractModel{
 
             }
 
-            $product_field_ID = $data_schema['products']['fields'][$this->product_field_id];
-            $product_field_ID_catalogue = $data_schema['products']['fields'][$this->product_field_catalogue_id];
-
-            $data_schema['products']['fields'] = array($this->product_field_id => $product_field_ID, $this->product_field_catalogue_id => $product_field_ID_catalogue);
+            $product_field_ID_key = $product_field_ID_catalogue_key = ''; 
+            $product_field_ID_cont = $product_field_ID_catalogue_cont = array();
+            foreach ($data_schema['products']['fields'] as $key => $key_data){
+                if ($product_field_ID_key !== '' && $product_field_ID_catalogue_key !== '') break;
+                if ($product_field_ID_key == '' and $this->product_field_id == strtoupper($key)){
+                    $product_field_ID_key = $key;
+                    $product_field_ID_cont = $key_data;
+                    continue;
+                }
+                if ($product_field_ID_catalogue_key == '' and 
+                        (strtoupper($this->product_field_catalogue_id) == strtoupper($key) or 
+                        'CATALOGUE_ID' == strtoupper($key)
+                        )
+                    ){
+                    $product_field_ID_catalogue_key = $key;
+                    $product_field_ID_catalogue_cont = $key_data;
+                    continue;
+                }
+            }
+            
+            $data_schema['products']['fields'] = array($product_field_ID_key => $product_field_ID_cont, $product_field_ID_catalogue_key => $product_field_ID_catalogue_cont);
 
             foreach ($field_relations as $product_field => $format_field){
                 
